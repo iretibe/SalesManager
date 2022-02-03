6cd6734f60f54444d58cf4d55e4af3c313fe0788
Somad <iretibe@users.noreply.github.com>
2022-02-03 09:40:05 +0000

    Implemented the Commands

--- Services/SalesManager.Availability/SalesManager.Availability.Infrastructure/Extensions.cs
+++ Services/SalesManager.Availability/SalesManager.Availability.Infrastructure/Extensions.cs
@@ -1,6 +1,10 @@
 ﻿using Convey;
 using Convey.Persistence.MongoDB;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using SalesManager.Availability.Core.Repositories;
 using SalesManager.Availability.Infrastructure.Mongo.Documents;
+using SalesManager.Availability.Infrastructure.Mongo.Repositories;
 using System;
 
 namespace SalesManager.Availability.Infrastructure
@@ -9,11 +13,20 @@ namespace SalesManager.Availability.Infrastructure
     {
         public static IConveyBuilder AddInfrastructure(this IConveyBuilder builder)
         {
+            builder.Services.AddTransient<IResourcesRepository, ResourcesMongoRepository>();
+
             builder
                 .AddMongo()
                 .AddMongoRepository<ResourceDocument, Guid>("resources");
 
             return builder;
+        }
+
+        public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+        {
+            app.UseConvey();
+
+            return app;
         }
     }
 }
